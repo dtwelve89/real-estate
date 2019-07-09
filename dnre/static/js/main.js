006be326795c8c26c47035a36b2a5c8b9a600448
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-07-08 19:18:05 -0700

    User Registration

--- dnre/static/js/main.js
+++ dnre/static/js/main.js
@@ -2,5 +2,5 @@ const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
 setTimeout(function() {
-  $('#message').fadeout('slow');
+  $('#message').fadeout('slow')
 }, 3000);
