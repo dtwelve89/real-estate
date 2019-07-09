2c3771cdef4172206747b5ffe998e0028c7afd8e
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-07-09 12:30:31 -0700

    Inquiry Check & Send Email

--- dnre/settings.py
+++ dnre/settings.py
@@ -142,3 +142,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 
 }
+
+# Email Config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'test@gmail.com'
+# EMAIL_HOST_PASSWORD='secret'
+# EMAIL_USE_TLS=True
