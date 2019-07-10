75efb36d8f100eccb944221ba2219a4e2b4cda66
Danny Nguyen <danny.danh.nguyen@gmail.com>
2019-07-10 11:23:47 -0700

    Added Local Settings Import

--- dnre/settings.py
+++ dnre/settings.py
@@ -149,3 +149,8 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_USER = 'test@gmail.com'
 # EMAIL_HOST_PASSWORD='secret'
 # EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
